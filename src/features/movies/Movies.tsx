c43614151978756977cdec20efef0fe72e69ca6b
Jered <Jered.arias25gmail.com>
2026-02-11 14:35:16 -0600

    Terminado

--- src/features/movies/Movies.tsx
+++ src/features/movies/Movies.tsx
@@ -11,7 +11,7 @@ export default function Movies() {
         getMovies()
         .then(setMovies)
         .finally(() => setLoading(false));
-    }, [])
+    }, [movies])
 
     if (loading) {
         return (
